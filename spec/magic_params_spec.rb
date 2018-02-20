432488d8a22a57ed22daaedf02609dad44d5fbc9
Chase <chase.gilliam@gmail.com>
2018-02-20 17:30:39 -0500

    fixed test naming

--- spec/magic_params_spec.rb
+++ spec/magic_params_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe AllscriptsApi::NumberedParams do
+RSpec.describe AllscriptsApi::MagicParams do
   describe "format" do
     let(:empty_params_hash) do
       {
@@ -18,12 +18,12 @@ RSpec.describe AllscriptsApi::NumberedParams do
     end
 
     it "builds a blank params hash" do
-      expect(AllscriptsApi::NumberedParams.format).to eq(empty_params_hash)
+      expect(AllscriptsApi::MagicParams.format).to eq(empty_params_hash)
     end
 
     it "builds a params hash from partial set of params" do
       expect(
-        AllscriptsApi::NumberedParams.format(parameter4: "some_data")
+        AllscriptsApi::MagicParams.format(parameter4: "some_data")
       ).to eq(filled_params)
     end
   end
