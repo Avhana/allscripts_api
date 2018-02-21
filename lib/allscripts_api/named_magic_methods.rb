a35b3d51a67389814208c6c20e657779638efbf4
Chase <chase.gilliam@gmail.com>
2018-02-21 15:06:12 -0500

    api tweak and get_patient_problems

--- lib/allscripts_api/named_magic_methods.rb
+++ lib/allscripts_api/named_magic_methods.rb
@@ -12,8 +12,28 @@ module AllscriptsApi
           user_id: @allscripts_username,
           parameter1: search_string
         )
-      results = magic("SearchPatients", params: params)
+      results = magic("SearchPatients", magic_params: params)
       results["searchpatientsinfo"]
+    end
+
+    def get_patient_problems(patient_id,
+                             show_by_encounter = "N",
+                             assessed = nil,
+                             encounter_id = nil,
+                             filter_on_id = nil,
+                             display_in_progress = nil)
+      params =
+        MagicParams.format(
+          user_id: @allscripts_username,
+          patient_id: patient_id,
+          parameter1: show_by_encounter,
+          parameter2: assessed,
+          parameter3: encounter_id,
+          parameter4: filter_on_id,
+          parameter5: display_in_progress
+        )
+      results = magic("GetPatientProblems", magic_params: params)
+      results ["getpatientproblemsinfo"]
     end
   end
 end
