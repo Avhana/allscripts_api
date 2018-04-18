78c8a092775ef27655623111ecab86ccbd135265
Chase <chase.gilliam@gmail.com>
2018-04-18 15:36:55 -0400

    first pass at SaveOrder and XML builder

---
+++ lib/allscripts_api/order.rb
@@ -0,0 +1,40 @@
+module Allscripts
+  class Order
+    def self.build_xml(site_id, emr_user_id, order_date, encounter_id = "")
+      date = order_date.strftime("%d-%b-%Y")
+      builder = Nokogiri::XML::Builder.new
+
+      builder.saveorderxml do
+        # a value of 'Y' makes the order visible on the order list
+        builder.field("id" => "ordershowonorderlist", "value" => "Y")
+        builder.field("id" => "orderlocation", "value" => site_id)
+        builder.field("id" => "ordertobedone", "value" => date)
+        # default, not important, required
+        builder.field("id" => "orderpriority", "value" => "2")
+        builder.field("id" => "orderschedulefreetext", "value" => "")
+        builder.field("id" => "orderschedulerecurring", "value" => "")
+        builder.field("id" => "orderschedulerecurringperiod", "value" => "")
+        builder.field("id" => "orderschedulestarting", "value" => date)
+        builder.field("id" => "orderscheduleending", "value" => date)
+        builder.field("id" => "orderscheduleendingafter", "value" => "")
+        builder.field("id" => "orderperformingcomment", "value" => "")
+        builder.field("id" => "ordercommunicatedby", "value" => 11) # hardcoded
+        builder.field("id" => "orderorderedby", "value" => emr_user_id)
+        builder.field("id" => "ordermanagedby", "value" => emr_user_id)
+        builder.field("id" => "ordersupervisedby", "value" => "")
+        builder.field("id" => "orderciteresult", "value" => "")
+        builder.field("id" => "orderannotation", "value" => "")
+        builder.field("id" => "orderstatus", "value" => "Active")
+        builder.field("id" => "orderstatusdate", "value" => date)
+        builder.field("id" => "orderdeferralinterval", "value" => 0)
+        builder.field("id" => "orderdeferralunit", "value" => "Days")
+        builder.field("id" => "orderstatusreason", "value" => "")
+        builder.field("id" => "encounter", "value" => encounter_id.to_s)
+        builder.orderlinkedproblems
+        builder.clinicalquestions
+      end
+
+      builder.to_xml
+    end
+  end
+end
---
+++ lib/allscripts_api/ordering_methods.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module AllscriptsApi
+  # A collection convenience methods for ordering that map
+  # to Allscripts magic actions. Some of these methods
+  # wrap multiple actions. These methods are included
+  # in `AllscriptsApi::Client` and can be accessed from
+  # instances of that class.
+  module OrderingMethods
+    # a wrapper around SaveOrder, which save and order of the 
+    # sepcified category, among: AdministeredMedication,
+    # Immunization, InstructionOrder, ProcedureOrder,
+    # Referral, SuppliesOrder.
+    #
+    # xml can be constructed with `Allscripts::Order.build_xml`
+    #
+    # @param xml [String] xml containing saveorderxml fields and values
+    # @param order_category [String] one of AdministeredMedication,
+    # Immunization, InstructionOrder, ProcedureOrder,
+    # Referral, SuppliesOrder.
+    # @param dictionary_id [String] id of the dictionary from SearchOrder
+    # @param problem_id [String|Nil] problem to associate with the order, 
+    # may be passed in as part of the xml
+    # @param trans_id [String|Nil] the original OrderID, 
+    # necessary when updating a past order
+    # @return ...
+    def save_order(xml, order_category, dictionary_id, problem_id = nil, trans_id = nil)
+      params = MagicParams.format(
+        user_id: @allscripts_username,
+        parameter1: xml,
+        parameter2: order_category,
+        parameter3: dictionary_id,
+        parameter4: problem_id,
+        parameter5: trans_id,
+      )
+      magic("SaveOrder", magic_params: params)
+    end
+
+    private
+
+    def format_date_range(start_date, end_date)
+      "#{start_date.strftime('%m/%d/%Y')}|#{end_date.strftime('%m/%d/%Y')}"
+    end
+  end
+end
